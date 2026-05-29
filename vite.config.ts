c9cbc0f6c7b94b34f73207b2a223a49e72a8750e
intsuc <s@intsuc.dev>
2026-05-29 23:56:16 +0900

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,23 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 
 import viteReact from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
-import { cloudflare } from "@cloudflare/vite-plugin"
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
     devtools(),
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tailwindcss(),
     tanstackStart({
       router: {
         quoteStyle: "double",
         semicolons: false,
+      },
+      prerender: {
+        enabled: true,
+        autoSubfolderIndex: true,
+        autoStaticPathsDiscovery: true,
+        crawlLinks: true,
+        failOnError: true,
       },
     }),
     viteReact(),
